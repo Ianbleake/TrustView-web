4cdb524a30020a10d7ae6879cc9c564150006095
IvanRangel <arviluki@gmail.com>
2026-02-08 21:03:49 -0600

    fix: scroll bar

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -5,7 +5,7 @@ import { AppToaster } from "@/components/AppToaster";
 export const App = (): React.ReactElement => {
   return (
     <main
-      className="min-h-screen w-screen flex flex-col bg-gray-50"
+      className="min-h-screen flex flex-col bg-gray-50"
       id="app-container"
     >
       <AppToaster />
