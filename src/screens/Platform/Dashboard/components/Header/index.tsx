f7edf1d2fcad97d4dcd1224134d79c7bf0b114dd
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2026-02-16 11:50:25 -0600

    feat: add dashboard reload button

--- src/screens/Platform/Dashboard/components/Header/index.tsx
+++ src/screens/Platform/Dashboard/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { BackGlow } from './BackGlow'
+import { Button } from '@/components/ui/button'
+import { RefreshCw } from 'lucide-react'
 
 export const Header = ():React.ReactElement => {
   return (
@@ -8,7 +10,15 @@ export const Header = ():React.ReactElement => {
       <span className='font-semibold uppercase text-amber-600'>👋 Bienvenido de vuelta</span>
       <h1 className='font-heading text-white text-3xl font-semibold '>Mi Tienda Nube</h1>
       <p className='text-gray-400 text-md font-normal'>Acá tenés un resumen de cómo van tus reseñas.</p>
-      
+      <Button
+        variant={"gradient"}
+        size={"sm"}
+        className='absolute right-6 top-1/2 -translate-y-1/2 flex items-center gap-2'
+        onClick={() => window.location.reload()}
+      >
+        Actualizar
+        <RefreshCw/>
+      </Button>
     </div>
   )
 }
