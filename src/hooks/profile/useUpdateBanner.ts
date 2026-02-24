774f2fb5b4b2def8a069062ba4db27248312b6d1
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2026-02-24 14:00:14 -0600

    feat: update banner hook to manage profile state on success

--- src/hooks/profile/useUpdateBanner.ts
+++ src/hooks/profile/useUpdateBanner.ts
@@ -1,14 +1,25 @@
 import { updateBanner } from "@/services/profile/updateBanner";
+import { useSessionStorage } from "@/storage/authStorage";
 import { useMutation, type UseMutationResult } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 export default function useUpdateBanner(): UseMutationResult<UpdateBannerResponde, AppError, UpdateBannerPayload> {
 
+  const { profile, updateProfile } = useSessionStorage();
+
   const updateBannerMutation = useMutation<UpdateBannerResponde, AppError, UpdateBannerPayload>({
     mutationKey: ["updateBanner"],
     mutationFn: updateBanner,
-    onSuccess: () => {
+    onSuccess: (updatedBanner:UpdateBannerResponde) => {
       toast.success("Banner actualizado correctamente");
+
+      const updatedProfile = {
+        ...profile,
+        banner: updatedBanner.data.banner,
+        bannerAccentColor: updatedBanner.data.banner_accent_color
+      } as Profile;
+
+      updateProfile(updatedProfile);
     },
     onError: () => {
       toast.error("Error al actualizar el banner");
