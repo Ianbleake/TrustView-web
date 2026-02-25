a6a8b1de6df7b9288e65116e183135753c4dc618
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2026-02-25 17:12:19 -0600

    feat: agregar enlace de producto con apertura en nueva pestaña

--- src/components/ReviewCard/index.tsx
+++ src/components/ReviewCard/index.tsx
@@ -59,7 +59,13 @@ export const ReviewCard = ({
             </div>
 
             <div className='border-t border-gray-200 pt-4 flex flex-row items-center justify-between'>
-                <p className='text-sm font-medium text-gray-500'>Producto: <a href={review.productUrl ?? "#"} className='text-orange-600'>{review.product}</a></p>
+                {
+                    review.productUrl ? (
+                        <p className='text-sm font-medium text-gray-500'>Producto: <a href={review.productUrl ?? "#"} target='_blank' className='text-orange-600'>{review.product}</a></p>
+                    ) : (
+                        <p className='text-sm font-medium text-gray-500'>Producto: <span className='text-orange-600'>{review.product}</span></p>
+                    )
+                }
                 {
                     review.status === "approved" ? (
                         <HideReview reviewId={review.id}/>
